534f0ded14efcf4cbe57bfdf85bd1a711489aa02
Unai Muñoz <unaimunoz2024@gmail.com>
2025-01-14 18:20:06 +0100

    Update

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,4 @@
-document.addEventListener('deviceready', onDeviceReady, false);
-
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {
-      // specify options here
-    });
+    M.Sidenav.init(document.querySelectorAll('.sidenav'));
+    M.Tabs.init(document.querySelectorAll('.tabs'), { swipeable: true });
 });
-
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-    // console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    // document.getElementById('deviceready').classList.add('ready');
-}
-
-window.onload = (event) => {
-    console.log("Pàgina carregada completament. Inicialitzant..");
-    var options = { "swipeable": true };
-    var el = document.getElementsByClassName('tabs');
-    var instance = M.Tabs.init(el, options);
-};
